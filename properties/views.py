d7191c6e10d5ddfa75263e0a14afca5ce8626ae6
MohamedAhmed-SUT <mohamedahmed2023sut@gmail.com>
2025-08-30 18:25:10 +0300

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
 from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
 from .models import Property
 
-@cache_page(60 * 15)  # 15 دقيقة
+@cache_page(60 * 15)  # كاش 15 دقيقة
 def property_list(request):
-    properties = Property.objects.all()
-    return render(request, "properties/property_list.html", {"properties": properties})
+    properties = Property.objects.all().values(
+        "id", "title", "description", "price", "location", "created_at"
+    )
+    return JsonResponse({"data": list(properties)})
